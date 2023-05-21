a33f3c2f5c46a48c438ada733c0e848b42a8cef0
leetwinoff <leetwinoff92@gmail.com>
2023-05-21 17:47:48 -0500

    Create Profile model for social media platform API

--- profile_services/models.py
+++ profile_services/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/", null=True, blank=True
+    )
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
