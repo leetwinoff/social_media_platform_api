b81fd49ce39b115d8d8577187aead951871e7d57
leetwinoff <leetwinoff92@gmail.com>
2023-05-21 18:41:39 -0500

    Added MEDIA_ROOT and MEDIA_URL configurations

--- social_media_platform_api/settings.py
+++ social_media_platform_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "user",
+    "profile_services",
 ]
 
 MIDDLEWARE = [
@@ -67,8 +68,6 @@ TEMPLATES = [
         },
     },
 ]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 WSGI_APPLICATION = "social_media_platform_api.wsgi.application"
@@ -120,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
