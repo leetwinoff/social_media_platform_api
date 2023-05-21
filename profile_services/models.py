b810eae17b193033006f73deaa64b6807299a17c
leetwinoff <leetwinoff92@gmail.com>
2023-05-21 18:09:52 -0500

    Create Comment model for social media platform API

--- profile_services/models.py
+++ profile_services/models.py
@@ -32,3 +32,13 @@ class Like(models.Model):
 
     def __str__(self):
         return f"Like by {self.user.username} on {self.post.id}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.id}"
