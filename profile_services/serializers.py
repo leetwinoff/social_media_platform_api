2b42362c7b2223f8e3c4d143d51525491f3baf54
leetwinoff <leetwinoff92@gmail.com>
2023-05-22 15:39:51 -0500

    Created PostSerializer and PostListSerializer

--- profile_services/serializers.py
+++ profile_services/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from profile_services.models import Profile
+from profile_services.models import Profile, Post
 from user.models import User
 from user.serializers import UserSerializer
 
@@ -35,6 +35,46 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 
 class ProfileListSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Profile
         fields = ("id", "user", "profile_picture", "bio")
+
+
+class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ("user", "post_image", "post_description", "created_at")
+
+    def create(self, validated_data):
+        user_data = validated_data.pop("user")
+        user = User.objects.create(**user_data)
+        post = Post.objects.create(user=user, **validated_data)
+        return post
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop("user")
+        user_serializer = self.fields["user"]
+        user_instance = instance.user
+
+        user_serializer.update(user_instance, user_data)
+
+        instance.post_image = validated_data.get("post_image", instance.post_image)
+        instance.post_description = validated_data.get(
+            "post_description", instance.post_description
+        )
+        instance.created_at = validated_data.get("created_at", instance.created_at)
+
+        instance.save()
+        return instance
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = ("user", "post_image", "post_description", "created_at")
