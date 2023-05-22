0aca17712ab3acf57b16e8b9e1703caaa2c05485
leetwinoff <leetwinoff92@gmail.com>
2023-05-22 14:48:08 -0500

    Create ProfileListSerializer & override create and update methods in ProfileSerializer

--- profile_services/serializers.py
+++ profile_services/serializers.py
@@ -17,3 +17,24 @@ class ProfileSerializer(serializers.ModelSerializer):
         user = User.objects.create(**user_data)
         profile = Profile.objects.create(user=user, **validated_data)
         return profile
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop("user")
+        user_serializer = self.fields["user"]
+        user_instance = instance.user
+
+        user_serializer.update(user_instance, user_data)
+
+        instance.profile_picture = validated_data.get(
+            "profile_picture", instance.profile_picture
+        )
+        instance.bio = validated_data.get("bio", instance.bio)
+
+        instance.save()
+        return instance
+
+
+class ProfileListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ("id", "user", "profile_picture", "bio")
