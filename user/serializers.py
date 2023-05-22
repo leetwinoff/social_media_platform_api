c6753d6936ad4c2f31205d7fb11a8013fb443c4d
leetwinoff <leetwinoff92@gmail.com>
2023-05-22 18:36:15 -0500

    Added CreateTokenView to obtain token and added  login endpoint and added AuthTokenSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from user.models import User
+
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,7 +14,10 @@ class UserSerializer(serializers.ModelSerializer):
             "password",
             "is_staff",
         )
-        read_only_fields = ("is_staff",)
+        read_only_fields = (
+            "id",
+            "is_staff",
+        )
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
@@ -25,3 +29,33 @@ class UserSerializer(serializers.ModelSerializer):
         if password:
             user.set_password(password)
             user.save()
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(label=(_("Email")), write_only=True)
+    password = serializers.CharField(
+        label=(_("Password")),
+        style={"input_type": "password"},
+        trim_whitespace=False,
+        write_only=True,
+    )
+    token = serializers.CharField(label=_("Token"), read_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = authenticate(
+                request=self.context.get("request"), email=email, password=password
+            )
+
+            if not user:
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
+        else:
+            msg = _('Must include "email" and "password".')
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from user.views import CreateUserView, ManageUserView
+from user.views import CreateUserView, ManageUserView, CreateTokenView
+
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="token"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
