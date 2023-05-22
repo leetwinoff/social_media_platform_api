aa92f0140602a37725e6aa257c6f2ebab6ce1209
leetwinoff <leetwinoff92@gmail.com>
2023-05-22 14:56:07 -0500

    Overrided get_serializer_class method

--- profile_services/views.py
+++ profile_services/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
 from profile_services.models import Profile
-from profile_services.serializers import ProfileSerializer
+from profile_services.serializers import ProfileSerializer, ProfileListSerializer
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProfileListSerializer
+        return ProfileSerializer
