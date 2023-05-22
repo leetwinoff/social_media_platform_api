a16f262373ad39155e948f1bb74411c6ee85583a
leetwinoff <leetwinoff92@gmail.com>
2023-05-22 15:41:05 -0500

    Created endpoint for PostViewSet

--- profile_services/urls.py
+++ profile_services/urls.py
@@ -1,14 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import ProfileViewSet
-
+from .views import ProfileViewSet, PostViewSet
 
 router = DefaultRouter()
 router.register("profile", ProfileViewSet)
+router.register("post", PostViewSet)
 
 urlpatterns = [
     # Other URL patterns
-    path("profile/", include(router.urls)),
+    path("", include(router.urls)),
+    path("", include(router.urls)),
 ]
 
 app_name = "profile_services"
