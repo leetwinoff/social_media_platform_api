2f92cc678a906f28ca59a33b73eea4d86c2c3249
leetwinoff <leetwinoff92@gmail.com>
2023-05-21 19:57:38 -0500

    Included user.urls and profile_services.urls path

--- social_media_platform_api/urls.py
+++ social_media_platform_api/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls", namespace="user")),
+    # path(
+    #     "api/profile_services/",
+    #     include("profile_services.urls", namespace="profile_services"),
+    # ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
