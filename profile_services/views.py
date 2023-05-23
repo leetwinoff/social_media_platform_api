00ad8507cf9eff3a1d7ebf03d86101b79f45af1b
leetwinoff <leetwinoff92@gmail.com>
2023-05-23 12:17:24 -0500

    Added action for PostViewset wich allows user to leave a comment under a post, added CommentViewSet

--- profile_services/views.py
+++ profile_services/views.py
@@ -1,9 +1,10 @@
-from rest_framework import viewsets, generics, status, mixins
+from rest_framework import viewsets, generics, status, mixins, serializers
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from profile_services.models import Profile, Post, Like
+from profile_services.models import Profile, Post, Like, Comment
 from profile_services.permissions import IsAdminOrIfAuthenticatedReadOnly
 from profile_services.serializers import (
     ProfileSerializer,
@@ -11,6 +12,7 @@ from profile_services.serializers import (
     PostSerializer,
     PostListSerializer,
     LikeSerializer,
+    CommentSerializer,
 )
 
 
@@ -18,22 +20,25 @@ class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
-
-    def get_permissions(self):
-        if self.action in (
-            "create",
-            "update",
-            "partial_update",
-            "destroy",
-        ):
-            return [IsAdminUser()]
-        return super().get_permissions()
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
         if self.action == "list":
             return ProfileListSerializer
         return ProfileSerializer
+
+    def perform_create(self, serializer):
+        user = self.request.user
+
+        if Profile.objects.filter(user=user).exists():
+            raise serializers.ValidationError("A profile already exists for this user.")
+
+        serializer.save(user=user)
+
+    def get_permissions(self):
+        if self.action == "create":
+            return []
+        return super().get_permissions()
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -44,6 +49,18 @@ class PostViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             return PostListSerializer
         return PostSerializer
+
+    @action(detail=True, methods=["POST"])
+    def add_comment(self, request, pk=None):
+        post = self.get_object()
+        serializer = CommentSerializer(
+            data=request.data, context={"request": request, "post": post}
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LikeViewSet(
@@ -75,3 +92,25 @@ class LikeViewSet(
             )
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        post_id = request.data.get("post_id")
+        try:
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return Response(
+                {"error": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=self.request.user, post=post)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
