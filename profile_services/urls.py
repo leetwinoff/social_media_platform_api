5b6a528faeefded7f73a29c8a5d75020983b5055
leetwinoff <leetwinoff92@gmail.com>
2023-05-23 12:18:08 -0500

    Registered router for CommentViewSet

--- profile_services/urls.py
+++ profile_services/urls.py
@@ -4,12 +4,14 @@ from profile_services.views import (
     ProfileViewSet,
     PostViewSet,
     LikeViewSet,
+    CommentViewSet,
 )
 
 router = DefaultRouter()
 router.register("profile", ProfileViewSet)
 router.register("post", PostViewSet)
 router.register("like", LikeViewSet)
+router.register("comment", CommentViewSet)
 
 urlpatterns = [
     # Other URL patterns
