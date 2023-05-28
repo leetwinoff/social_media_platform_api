f74d3bf34c554a7848a7a6d9b41cbb4941bb9364
leetwinoff <leetwinoff92@gmail.com>
2023-05-28 13:15:18 -0500

    Removed router for LikeViewSet

--- profile_services/urls.py
+++ profile_services/urls.py
@@ -3,14 +3,12 @@ from rest_framework.routers import DefaultRouter
 from profile_services.views import (
     ProfileViewSet,
     PostViewSet,
-    LikeViewSet,
     CommentViewSet,
 )
 
 router = DefaultRouter()
 router.register("profile", ProfileViewSet)
 router.register("post", PostViewSet)
-router.register("like", LikeViewSet)
 router.register("comment", CommentViewSet)
 
 urlpatterns = [
