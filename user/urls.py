f5599ee327536aadc837554095fb5dbf6fe0a512
leetwinoff <leetwinoff92@gmail.com>
2023-05-28 13:16:19 -0500

    Added log_out endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from user.views import CreateUserView, ManageUserView, CreateTokenView
-
+from user.views import CreateUserView, ManageUserView, CreateTokenView, LogOutView
 
 app_name = "user"
 
@@ -8,4 +7,5 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("login/", CreateTokenView.as_view(), name="token"),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path("log_out/", LogOutView.as_view(), name="log_out"),
 ]
