79b5686a25c0642f89bbb087134349dc16db289d
leetwinoff <leetwinoff92@gmail.com>
2023-05-28 19:30:03 -0500

    Created endpoint for TagViewSet

--- profile_services/urls.py
+++ profile_services/urls.py
@@ -3,12 +3,14 @@ from rest_framework.routers import DefaultRouter
 from profile_services.views import (
     ProfileViewSet,
     PostViewSet,
+    TagViewSet,
     # CommentViewSet,
 )
 
 router = DefaultRouter()
 router.register("profile", ProfileViewSet)
 router.register("post", PostViewSet)
+router.register("tag", TagViewSet)
 
 
 urlpatterns = [
