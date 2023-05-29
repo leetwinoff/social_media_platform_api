997e5a227ecf7e1a83967acf162deda9065048d6
leetwinoff <leetwinoff92@gmail.com>
2023-05-28 19:29:43 -0500

    Imported dotenv

--- social_media_platform_api/settings.py
+++ social_media_platform_api/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+import dotenv
+
+dotenv.load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ng@qi(m*-aw$(!5*&&sb&0l)(hksv2g&2jok&70vn**i(q1)&5"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
