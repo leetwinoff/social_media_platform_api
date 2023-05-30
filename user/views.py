261da1182fe483d3d0888375abc9b365f01eff61
leetwinoff <leetwinoff92@gmail.com>
2023-05-30 15:54:10 -0500

    Updated token delete method in LogOutView

--- user/views.py
+++ user/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.generics import get_object_or_404
+
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
@@ -33,8 +35,6 @@ class LogOutView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
-        try:
-            Token.objects.get(user=request.user).delete()
-        except Token.DoesNotExist:
-            pass
+        token = get_object_or_404(Token, user=request.user)
+        token.delete()
         return Response({"detail": "Succesfully log out"}, status=status.HTTP_200_OK)
